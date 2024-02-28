ba265970c36412197e5f9c7940d445700e4ccdd5
DevHyperCoder <devan@devhypercoder.com>
2024-02-28 10:04:08 +0530

    mass composition

--- src/stats.rs
+++ src/stats.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use atomio::parser::Parsed;
 use yew::prelude::*;
 
-use crate::utils::calculate_mass;
+use crate::utils::{calculate_mass, get_mass_composition};
 
 #[derive(Debug,Clone,Properties,PartialEq)]
 pub struct StatsProps {
@@ -24,14 +24,27 @@ pub fn Stats(props: &StatsProps) -> Html {
         comps.insert(k, (val as f64) * 100. / (sum as f64));
     }
 
+    let mass_composition = get_mass_composition(&parsed);
+
 
     html!(
         <section class="mt-5">
             <h2 class="text-slate-800 font-black text-3xl">{format!("Mass: {} g/mol",mass)}</h2>
-            <h2 class="text-slate-800 font-black text-3xl mt-3">{"Composition: "}</h2>
 
+            <h2 class="text-slate-800 font-black text-3xl mt-3">{"Formula Composition: "}</h2>
             <div class="flex flex-col gap-3 bg-white w-full rounded-md drop-shadow-xl mt-4 px-2 py-2">
                 {for comps.iter().map(|(k,v)| {
+                        html!(
+                            <div data-element={k.to_string()} data-comp={format!("{:.2}",v)} class="w-full flex gap-2 items-center asdf">
+                                <div class={format!("h-6 px-1 py-4 rounded-md bg-orange-500")} style={format!("width: {}%",v)}> </div>
+                            </div>
+                        )
+                })}
+            </div>
+
+            <h2 class="text-slate-800 font-black text-3xl mt-3">{"Mass Composition: "}</h2>
+            <div class="flex flex-col gap-3 bg-white w-full rounded-md drop-shadow-xl mt-4 px-2 py-2">
+                {for mass_composition.iter().map(|(k,v)| {
                         html!(
                             <div data-element={k.to_string()} data-comp={format!("{:.2}",v)} class="w-full flex gap-2 items-center asdf">
                                 <div class={format!("h-6 px-1 py-4 rounded-md bg-orange-500")} style={format!("width: {}%",v)}> </div>
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use atomio::parser::Parsed;
 use crate::data::ELEMENTS;
 
@@ -11,4 +13,18 @@ pub fn calculate_mass(p: &Parsed) -> f32 {
     }
 
     mass
+}
+
+pub fn get_mass_composition(p: &Parsed) -> HashMap<&String,f32> {
+    let mass = calculate_mass(&p);
+    let mut map = HashMap::new();
+    let formula_composition = p.root_unit.get_composition();
+    for (k, val) in formula_composition {
+        map.insert(
+            k, 
+            (val as f32) * ELEMENTS.get(k).unwrap() * 100. / mass
+        );
+    }
+
+    map
 }
